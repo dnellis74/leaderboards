c4b52fa854f7ff24c7a43d32fb98810e74d2c4dd
Dave Nellis <davedatasci@gmail.com>
2021-05-21 16:56:25 -0600

    Changed default scores in a tournament

--- src/main/java/com/winterbear/rangeishot/leaderboard/repo/entity/Tournament.java
+++ src/main/java/com/winterbear/rangeishot/leaderboard/repo/entity/Tournament.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -29,6 +30,8 @@ public class Tournament {
     private LocalDate close;
     private String thumbnailPath;
     private String name;
+
+    @Column(columnDefinition="TEXT")
     private String description;
     private Integer numCourses;
     private String courses;
